ffc0c3bad6714395f526615b235e28b7ae669695
Chimezie Eguzo <c.eguzo@fz-juelich.de>
2026-09-15 16:53:36 +0200

    assignment 1

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -16,6 +16,7 @@ fi
 
 X=$(find "$filesdir" -type f  | wc -l)
 Y=$(grep -r "$searchstr" "$filesdir" | wc -l)
-echo "The number of files are ${X} and the number of matching lines are ${Y}"
+echo "The number of files are ${X} and "
+echo " sd the number of matching lines are ${Y}"
 
 exit 0
